25ce2ac854baa6b765ca4c7b8403306933348129
PeterCJ <pryrtcode@pryrt.com>
2025-05-14 14:08:47 -0700

    ConfigUpdater: grab stylers.model tree, and extract DefaultStyle’s fg/bgColor

--- src/Classes/ConfigUpdaterClass.cpp
+++ src/Classes/ConfigUpdaterClass.cpp
@@ -1,5 +1,25 @@
 #include "ConfigUpdaterClass.h"
 
+// delete null characters from padded wstrings				// private function (invisible to outside world)
+void delNull(std::wstring& str) {
+	const auto pos = str.find(L'\0');
+	if (pos != std::wstring::npos) {
+		str.erase(pos);
+	}
+};
+
+// convert wstring to UTF8-encoded bytes in std::string		// private function (invisible to outside world)
+std::string wstring_to_utf8(std::wstring& wstr) {
+	if (wstr.empty()) return std::string();
+	int szNeeded = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), static_cast<int>(wstr.size()), NULL, 0, NULL, NULL);
+	if (szNeeded == 0) return std::string();
+	std::string str(szNeeded, '\0');
+	int result = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), static_cast<int>(wstr.size()), const_cast<LPSTR>(str.data()), szNeeded, NULL, NULL);
+	if (result == 0) return std::string();
+	return str;
+}
+
+
 ConfigUpdater::ConfigUpdater(HWND hwndNpp) {
 	_hwndNPP = hwndNpp;
 	_populateNppDirs();
@@ -10,18 +30,11 @@ void ConfigUpdater::_initInternalState(void) {
 	_wsSavedComment = L"";
 	_bHasTopLevelComment = false;
 	//treeModel -- was an ETree::parse output object, but I'm not sure tinyxml2 needs such an intermediary... TBD
-	_mapModelDefaultColors[L"fgColor"] = L"";
-	_mapModelDefaultColors[L"bgColor"] = L"";
+	_mapModelDefaultColors["fgColor"] = "";
+	_mapModelDefaultColors["bgColor"] = "";
 }
 
 void ConfigUpdater::_populateNppDirs(void) {
-	auto delNull = [](std::wstring& str) {
-		const auto pos = str.find(L'\0');
-		if (pos != std::wstring::npos) {
-			str.erase(pos);
-		}
-		};
-
 	// %AppData%\Notepad++\Plugins\Config or equiv
 	LRESULT sz = 1 + ::SendMessage(_hwndNPP, NPPM_GETPLUGINSCONFIGDIR, 0, NULL);
 	std::wstring pluginCfgDir(sz, '\0');
@@ -54,6 +67,7 @@ void ConfigUpdater::_populateNppDirs(void) {
 	std::wstring exeDir(MAX_PATH, '\0');
 	::SendMessage(_hwndNPP, NPPM_GETNPPDIRECTORY, MAX_PATH, reinterpret_cast<LPARAM>(exeDir.data()));
 	delNull(exeDir);
+	_nppAppDir = exeDir;
 	_nppCfgAutoCompletionDir = exeDir + L"\\autoCompletion";
 
 	return;
@@ -62,5 +76,41 @@ void ConfigUpdater::_populateNppDirs(void) {
 void ConfigUpdater::go(void)
 {
 	_initInternalState();
+	_getModelStyler();
+	return;
+}
+
+void ConfigUpdater::_getModelStyler(void)
+{
+	//_stylers_model_xml.oDoc;
+	_stylers_model_xml.fName = std::wstring(MAX_PATH, L'\0');
+	PathCchCombine(const_cast<PWSTR>(_stylers_model_xml.fName.data()), MAX_PATH, _nppAppDir.c_str(), L"stylers.model.xml");
+	delNull(_stylers_model_xml.fName);
+	tinyxml2::XMLError eResult = _stylers_model_xml.oDoc.LoadFile(wstring_to_utf8(_stylers_model_xml.fName).c_str());
+	_xml_check_result(eResult, &_stylers_model_xml.oDoc);
+	_stylers_model_xml.pRoot = _stylers_model_xml.oDoc.RootElement();
+	if (_stylers_model_xml.pRoot == NULL) {
+		_xml_check_result(tinyxml2::XML_ERROR_FILE_READ_ERROR, &_stylers_model_xml.oDoc);
+	}
+
+	// tinyxml2 doesn't have XPath, and my attempts at adding on tixml2ex.h's find_element() couldn't seem to ever find any element, even the root
+	//	so switch to manual navigation.
+	tinyxml2::XMLElement* pSearch = _stylers_model_xml.oDoc.FirstChildElement("NotepadPlus");
+	if (pSearch) pSearch = pSearch->FirstChildElement("GlobalStyles");
+	if (pSearch) pSearch = pSearch->FirstChildElement("WidgetStyle");
+	while (pSearch) {
+		if (pSearch->Attribute("styleID", "32")) {	// name="Default Style" styleID="32"
+			break;
+		}
+		pSearch = pSearch->NextSiblingElement();
+	}
+	if (pSearch) {
+		_mapModelDefaultColors["fgColor"] = pSearch->Attribute("fgColor");
+		_mapModelDefaultColors["bgColor"] = pSearch->Attribute("bgColor");
+	}
+	else {
+		_xml_check_result(tinyxml2::XML_ERROR_PARSING_ELEMENT, &_stylers_model_xml.oDoc);
+	}
+
 	return;
 }
--- src/Classes/ConfigUpdaterClass.h
+++ src/Classes/ConfigUpdaterClass.h
@@ -18,13 +18,33 @@ private:
 	std::wstring _wsSavedComment;
 	bool _bHasTopLevelComment;
 	//treeModel -- was an ETree::parse output object, but I'm not sure tinyxml2 needs such an intermediary... TBD
-	std::map<std::wstring, std::wstring> _mapModelDefaultColors;
+	std::map<std::string, std::string> _mapModelDefaultColors;
 
-	void _initInternalState(void);	// sets internal attributes back to default
+	void _initInternalState(void);			// sets internal attributes back to default
+	void _getModelStyler(void);				// gets the XML
+
+	// keeps track of the tinyxml2 Document and Root Node for a given XML file
+	struct stXml {
+		tinyxml2::XMLDocument oDoc;
+		tinyxml2::XMLNode* pRoot = NULL;
+		std::wstring fName;
+	};
+
+	stXml _stylers_model_xml;				// need access to the stylers.model.xml structure throughout
+	tinyxml2::XMLError _xml_check_result(tinyxml2::XMLError a_eResult, tinyxml2::XMLDocument* p_doc = NULL) {
+		if (a_eResult != tinyxml2::XML_SUCCESS) {
+			std::string sMsg = std::string("XML Error #") + std::to_string(static_cast<int>(a_eResult));
+			if (p_doc != NULL) sMsg += std::string(": ") + std::string(p_doc->ErrorStr());
+			::MessageBoxA(NULL, sMsg.c_str(), "XML Error", MB_ICONWARNING | MB_OK);
+		}
+		return a_eResult;
+	};
+
 
 	// Npp Metadata
 	void _populateNppDirs(void);
 	std::wstring
+		_nppAppDir,
 		_nppCfgDir,
 		_nppCfgUdlDir,
 		_nppCfgFunctionListDir,
