b90f48e14b778080791d8675dffb02d22d724dc4
PeterCJ <pryrtcode@pryrt.com>
2025-05-29 08:08:40 -0700

    ConfigUpdater: v1.99_beta3 = Workflow should properly make/append zipfile during tag/release

--- src/Dialogs/PluginVersion.h
+++ src/Dialogs/PluginVersion.h
@@ -18,8 +18,8 @@
 */
 
 #pragma once
-#define VERSION_DIGITALVALUE 1, 99, 0, 2
-#define VERSION_VALUE "1.99_beta2\0"
+#define VERSION_DIGITALVALUE 1, 99, 0, 3
+#define VERSION_VALUE "1.99_beta3\0"
 #define VERSION_WSTR TEXT(VERSION_VALUE)
 #define VERSION_AUTHOR "Peter C. Jones\0"
 #define VERSION_PLUGIN_DESCRIPTION "Notepad++ Plugin to Keep Langs/Stylers/Themes Config Files Up-to-Date\0"
