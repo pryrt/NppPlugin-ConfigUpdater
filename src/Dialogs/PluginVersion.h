226d2c6d8bcb3d3a9adfc5a3121184452c9ab052
PeterCJ <pryrtcode@pryrt.com>
2025-06-02 07:11:47 -0700

    ConfigUpdater v2.0.0: Ready for initial plugin release

--- src/Dialogs/PluginVersion.h
+++ src/Dialogs/PluginVersion.h
@@ -18,8 +18,8 @@
 */
 
 #pragma once
-#define VERSION_DIGITALVALUE        1, 99, 0, 6
-#define VERSION_VALUE               "1.99_beta6\0"
+#define VERSION_DIGITALVALUE        2, 0, 0, 0
+#define VERSION_VALUE               "2.0.0\0"
 #define VERSION_WSTR                TEXT(VERSION_VALUE)
 #define VERSION_AUTHOR              "Peter C. Jones\0"
 #define VERSION_PLUGIN_DESCRIPTION  "Notepad++ Plugin to Keep Langs/Stylers/Themes Config Files Up-to-Date\0"
