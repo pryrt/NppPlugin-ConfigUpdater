7a8b2ad45c298065e499a9b9a4d8ad955ec2848d
PeterCJ <pryrtcode@pryrt.com>
2025-06-25 15:28:38 -0700

    ConfigUpdater: Validation Dialog = get the Npp metadata needed; populate object attribute vectors for the lists of XML/XSD [TODO: accessor to those lists, and use xmlNames to populate combobox]

--- src/Classes/ConfigValidatorClass.h
+++ src/Classes/ConfigValidatorClass.h
@@ -5,23 +5,53 @@
 #include <vector>
 #include <map>
 #include <windows.h>
-//#include <pathcch.h>
-//#include <shlwapi.h>
+#include <pathcch.h>
+#include <shlwapi.h>
 #include "PluginDefinition.h"
 #include "menuCmdID.h"
-//#include "tinyxml2.h"
 #include "CUValidationDialog.h"
-//#include <comutil.h>
 
 class ConfigValidator {
 public:
-	ConfigValidator(HWND hwndNpp, HWND hwndFileCbx, HWND hwndErrLbx);
+	ConfigValidator(NppData& origNppData);
 	void go(bool isIntermediateSorted);
 
 private:
+	void _populateValidationLists(void);		// create the list/mapping of XML and XSD
+	std::vector<std::wstring>	// all share the same index number, so index i of each array will be the same value
+		_xmlNames,								// vector to store the short name for each XML to validate
+		_xmlPaths,								// vector to store the full path for each XML to validate
+		_xsdPaths;								// vector to store the full path for each XSD to use for validation
+
+	////////////////////////////////
+	// Private Helpers
+	////////////////////////////////
+
+	// delete NULL characters at end of wstring
+	std::wstring _delNull(std::wstring& str);
+
+	// get the active scintilla handle
+	HWND _hwActiveScintilla(void);
+
+	////////////////////////////////
+	// Npp Metadata
+	////////////////////////////////
+	void _populateNppMetadata(NppData& origNppData);	// using the HWND information, poll N++ for the correct directory information and other metadata
 	struct {
-		HWND npp;
-		HWND fileCbx;
-		HWND errLbx;
-	} _hw;
+		NppData hwnd;
+		struct {
+			std::wstring
+				app,
+				appThemes,
+				appExePath,
+				cfg,
+				cfgPluginConfig,
+				cfgPluginConfigMyDir,
+				cfgUdl,
+				cfgFunctionList,
+				cfgAutoCompletion,
+				cfgThemes;
+		} dir;
+	} _npp;
+
 };
