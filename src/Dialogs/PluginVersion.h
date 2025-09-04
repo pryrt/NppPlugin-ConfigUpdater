ffb01c150a19c13638ef7c6aba1c4ba316090fd0
PeterCJ <pryrtcode@pryrt.com>
2025-09-04 14:14:39 -0700

    uprev to 2.2.1-dev3 before merge

--- src/Dialogs/PluginVersion.h
+++ src/Dialogs/PluginVersion.h
@@ -18,8 +18,8 @@
 */
 
 #pragma once
-#define VERSION_DIGITALVALUE        2, 2, 1, 2
-#define VERSION_VALUE               "2.2.1-dev2\0"
+#define VERSION_DIGITALVALUE        2, 2, 1, 3
+#define VERSION_VALUE               "2.2.1-dev3\0"
 #define VERSION_WSTR                TEXT(VERSION_VALUE)
 #define VERSION_AUTHOR              "Peter C. Jones\0"
 #define VERSION_PLUGIN_DESCRIPTION  "Notepad++ Plugin to Keep Langs/Stylers/Themes Config Files Up-to-Date\0"
